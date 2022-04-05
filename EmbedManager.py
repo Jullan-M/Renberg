620d5e40e668660e9f2efa2ebcc7fc3d83927f91
Jullan-M <jmihkkal_lindi@hotmail.com>
2022-04-05 21:21:38 +0200

    first commit

---
+++ EmbedManager.py
@@ -0,0 +1,148 @@
+import asyncio
+import discord
+import json
+import re
+import requests
+from discord.ext import commands
+
+def load_json(filename: str):
+    with open(filename, "r", encoding="utf-8") as f:
+        js = json.load(f)
+    return js
+
+MAX_EMBED_LENGTH = 4096
+MULTIPAGE_TIMEOUT = 900 # Timeout period for page flipping with reacts
+
+class EmbedManager(commands.Cog, name='EmbedManager'):
+    def __init__(self, bot):
+        self.bot = bot
+        self.multipage_timeout = MULTIPAGE_TIMEOUT        
+
+    @staticmethod
+    def generate_embed(title, passage, author_data, passage_url, color):
+        embed = discord.Embed(title=title, description=passage, url=passage_url, color=color)
+        embed.set_author(name=author_data["name"], url=author_data["url"], icon_url=author_data["icon"])
+        # embed.set_thumbnail(url=author_data["thumbnail"])   
+        return embed
+
+    async def multi_page(self, ctx, embeds):
+        pages = len(embeds)
+        cur_page = 0
+        for i, em in enumerate(embeds):
+            pg = i + 1
+            em.set_footer(text=f"Page {pg} of {pages}")
+
+        message = await ctx.send(embed=embeds[0])
+        
+        await message.add_reaction("◀️")
+        await message.add_reaction("▶️")
+
+        valid_emoji = ["◀️", "▶️", "🗑️"]
+        def check(reaction, user):
+            # Make sure nobody except the command sender can interact with the "menu"
+            # The user can't flip pages in multiple messages at once either
+            nonlocal message
+            return user == ctx.author and reaction.message.id==message.id and str(reaction.emoji) in valid_emoji
+            
+
+        while True:
+            try:
+                # wait for a reaction to be added
+                # times out after MULTIPAGE_TIMEOUT seconds
+                reaction, user = await self.bot.wait_for("reaction_add", timeout=self.multipage_timeout, check=check)
+                
+                if str(reaction.emoji) == "▶️" and cur_page != pages-1:
+                    cur_page += 1
+                    await message.edit(embed=embeds[cur_page])
+                    await message.remove_reaction(reaction, user)
+
+                elif str(reaction.emoji) == "◀️" and cur_page > 0:
+                    cur_page -= 1
+                    await message.edit(embed=embeds[cur_page])
+                    await message.remove_reaction(reaction, user)
+                elif str(reaction.emoji) == "🗑️":
+                    # Bot messages can be deleted by reacting with the waste basket emoji
+                    await message.delete()
+                    break
+                else:
+                    # remove reactions if the user tries to go forward on the last page or
+                    # backwards on the first page
+                    await message.remove_reaction(reaction, user)
+            except (asyncio.TimeoutError, discord.errors.Forbidden):
+                # end the loop if user doesn't react after MULTIPAGE_TIMEOUT seconds
+                for r in valid_emoji: await message.remove_reaction(r, self.bot.user)
+                await message.clear_reactions()
+                break
+
+    async def deletables(self, ctx, messages):
+        # Makes messages deletable by reacting wastebasket on them
+        # Check mark reacts make reactions go away (but one may still delete them!)
+        last_message = messages[-1]
+
+        await last_message.add_reaction("✅")
+        await last_message.add_reaction("🗑️")
+
+        valid_emoji = ["✅", "🗑️"]
+        def check(reaction, user):
+            # Make sure nobody except the command sender can interact with the "menu"
+            # The user can't flip pages in multiple messages at once either
+            nonlocal last_message
+            return user == ctx.author and reaction.message.id==last_message.id and str(reaction.emoji) in valid_emoji
+
+        while True:
+            try:
+                # wait for a reaction to be added
+                # times out after MULTIPAGE_TIMEOUT seconds
+                reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
+                
+                if str(reaction.emoji) == "✅":
+                    await last_message.clear_reactions()
+                elif str(reaction.emoji) == "🗑️":
+                    for m in messages: await m.delete()
+                    break
+                else:
+                    # remove reactions if the user tries to go forward on the last page or
+                    # backwards on the first page
+                    await last_message.remove_reaction(reaction, user)
+            except (asyncio.TimeoutError, discord.errors.Forbidden):
+                # end the loop if user doesn't react after MULTIPAGE_TIMEOUT seconds
+                for r in valid_emoji: await last_message.remove_reaction(r, self.bot.user)
+                await last_message.clear_reactions()
+                break
+
+    @commands.command(name='create_embed', help="Creates an embed based on a json file.")
+    async def create_embed(self, ctx, channel_id: int):
+        if not ctx.message.attachments:
+            await ctx.send("You need to attach a valid json file.")
+        try:
+            attachment_url = ctx.message.attachments[0].url
+            file_request = requests.get(attachment_url)
+            json_code = json.loads(file_request.text)
+        except ValueError:
+            await ctx.send("The file was not a valid json file.")
+            return
+        
+        channel = self.bot.get_channel(channel_id)
+        embed = discord.Embed.from_dict(json_code)
+        await channel.send(embed=embed)
+
+    @commands.command(name='edit_embed', help="Edit an already existing embed based on a json file.")
+    async def edit_embed(self, ctx, channel_id: int, message_id: int):
+        if not ctx.message.attachments:
+            await ctx.send("You need to attach a valid json file.")
+        try:
+            attachment_url = ctx.message.attachments[0].url
+            file_request = requests.get(attachment_url)
+            json_code = json.loads(file_request.text)
+        except ValueError:
+            await ctx.send("The file was not a valid json file.")
+            return
+        
+        channel = self.bot.get_channel(channel_id)
+        msg = await channel.fetch_message(message_id)
+        embed = discord.Embed.from_dict(json_code)
+        await msg.edit(embed=embed)
+
+def setup(bot):
+    bot.add_cog(EmbedManager(bot))
+    print("EmbedManager cog up and ready!")
---
+++ run_bot.py
@@ -0,0 +1,53 @@
+import asyncio
+import os
+import sys
+import click
+from loguru import logger
+from dotenv import load_dotenv
+from discord.ext import commands
+from EmbedManager import EmbedManager
+
+load_dotenv(dotenv_path='.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix="]")
+bot.add_cog(EmbedManager(bot))
+
+def is_guild_owner():
+    def predicate(ctx):
+        return ctx.guild is not None and ctx.guild.owner_id == ctx.author.id
+    return commands.check(predicate)
+
+@bot.command(name='add_cog')
+@commands.check_any(commands.is_owner(), is_guild_owner())
+async def add_cog(ctx, cog_name: str):
+    bot.load_extension(cog_name)
+    logger.success(f"Loaded cog: {cog_name}")
+    await ctx.send(f"Added cog {cog_name}.")
+
+@bot.command(name='remove_cog')
+@commands.check_any(commands.is_owner(), is_guild_owner())
+async def remove_cog(ctx, cog_name: str):
+    bot.remove_cog(cog_name)
+    logger.success(f"Removed cog: {cog_name}")
+    await ctx.send(f"Removed cog {cog_name}.")
+
+@click.command()
+@click.option(
+    "-d",
+    "--debug",
+    is_flag=True,
+    help="Enable debug mode. Prints debug messages to the console.",
+)
+def main(debug: bool):
+    if not debug:
+        # Default loguru level is DEBUG
+        logger.remove()
+        logger.add(os.sys.stderr, level="INFO")
+    
+    bot.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
+    
